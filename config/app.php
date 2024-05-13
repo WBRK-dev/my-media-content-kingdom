6a845ea491ffe8d587e6ef0581e2caf6dae28646
WBRK-dev <koertjrweberp@gmail.com>
2024-05-13 15:44:40 +0200

    feat: added short name to config

--- config/app.php
+++ config/app.php
@@ -17,6 +17,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'short_name' => env('APP_NAME_SHORT', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
