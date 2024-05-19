3f2c0be42c2dc8fc2f0c8da3cfc32d813066b3b4
WBRK-dev <koertjrweberp@gmail.com>
2024-05-19 15:55:43 +0200

    feat: added the ability to change video resolution

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -4,35 +4,38 @@
     
     <video controls muted></video>
 
-    
+    <select onchange="updateResolution(this)"></select>
 
 @endsection
 
 @section('head')
     <script src="https://cdn.jsdelivr.net/npm/hls.js@1.5.8/dist/hls.min.js"></script>
     <script>
-        window.onload = ()=> {
+        let hls;
+        document.addEventListener("DOMContentLoaded", () => {
             var video = document.querySelector('video');
             var videoSrc = '{{ config("app.url") }}/watch/{{$video->id}}/index.m3u8';
             if (Hls.isSupported()) {
-                const config = {
-                    startPosition: 0
-                }
-                var hls = new Hls(config);
+
+                const config = { startPosition: 0 };
+                hls = new Hls(config);
                 hls.loadSource(videoSrc);
                 hls.attachMedia(video);
                 video.play();
-            }
-            // HLS.js is not supported on platforms that do not have Media Source
-            // Extensions (MSE) enabled.
-            //
-            // When the browser has built-in HLS support (check using `canPlayType`),
-            // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-            // element through the `src` property. This is using the built-in support
-            // of the plain video element, without using HLS.js.
-            else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                video.src = videoSrc;
-            }
+
+                hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+                    let resolutionSelectorElem = document.querySelector("select");
+                    resolutionSelectorElem.innerHTML = "";
+                    for (let i = 0; i < hls.levels.length; i++) {
+                        resolutionSelectorElem.innerHTML += `<option value="${i}">${hls.levels[i].height}p</option>`;
+                    }
+                });
+
+            } else if (video.canPlayType('application/vnd.apple.mpegurl')) { video.src = videoSrc; video.play(); }
+        });
+
+        function updateResolution(elem) {
+            hls.currentLevel = Number(elem.value);
         }
     </script>
 @endsection
