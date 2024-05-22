7d4f38feaeca1a734c6088cd93996eb35fc4080e
WBRK-dev <koertjrweberp@gmail.com>
2024-05-22 11:10:12 +0200

    chore: added home link to sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,7 +1,3 @@
 <div class="sidebar">
-    <h2>this is the sidebar</h2>
-    <a href="#">This a placeholder link</a><br>
-    <a href="#">This a placeholder link</a><br>
-    <a href="#">This a placeholder link</a><br>
-    <a href="#">This a placeholder link</a>
+    <a href="{{ config("app.url") }}">Home</a>
 </div>
