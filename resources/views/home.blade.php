c3a7b932f3173e95eb38dbad8e76424660024f5b
WBRK-dev <koertjrweberp@gmail.com>
2024-05-22 10:46:55 +0200

    chore: basic styling home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,23 @@
 @extends('layout.root')
 
 @section('body')
-    <main class="d-flex f-col align-c">
+    <main class="d-flex flex-column align-items-center gap-2 p-2">
         <div class="ad-banner debugborder">
             <p>This is a placeholder for an ad banner</p>
         </div>
-        <div class="videos-grid">
+        <video-grid class="w-100">
             @foreach ($videos as $video)
-                <a class="placeholder-video" href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
+                {{-- <a class="placeholder-video" href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
                     <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" style="width: 100%;">
                     <p> {{ $video->title }}</p>
+                </a> --}}
+                <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="video-grid-item">
+                    <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" style="width: 100%;">
+                    <div class="info">
+                        <p class="title">{{ $video->title }}</p>
+                    </div>
                 </a>
             @endforeach
-        </div>
+        </video-grid>
     </main>
 @endsection
