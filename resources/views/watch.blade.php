e512717210f0acce0f9864ef9dfd463b15c89245
milanh-dev <milanhaaijer2004@gmail.com>
2024-05-22 10:32:00 +0200

    feat: added views and viewcount

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -10,6 +10,12 @@
     
         <select onchange="updateResolution(this)" style="display: block;"></select>
 
+        @if ($video->getViews() == 1)
+            <div>{{$video->getViews()}} view</div>
+        @else
+            <div>{{$video->getViews()}} views</div>
+        @endif
+        
     </div></div>
 
 
