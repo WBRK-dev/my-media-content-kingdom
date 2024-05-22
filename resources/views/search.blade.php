983545a0f7f186c70de899cce7ed2374d272132b
WBRK-dev <koertjrweberp@gmail.com>
2024-05-22 11:29:03 +0200

    chore: basic styling search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,14 +2,16 @@
 
 @section('body')
 
-    <main>
-        <div class="videos-grid">
+    <main class="p-2">
+        <video-grid>
             @foreach ($videos as $video)
-                <a class="placeholder-video" href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
+                <a class="video-grid-item" href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
                     <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" style="width: 100%;">
-                    <p> {{ $video->title }}</p>
+                    <div class="info">
+                        <p class="title">{{ $video->title }}</p>
+                    </div>
                 </a>
             @endforeach
-        </div>
+        </video-grid>
     </main>
 @endsection
