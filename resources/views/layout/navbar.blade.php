6c46f88cbb694ec308377a16c4b6dfbc5fecf614
WBRK-dev <koertjrweberp@gmail.com>
2024-05-22 11:04:46 +0200

    feat: added logo button to home

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,4 +1,7 @@
 <div class="topnavbar">
+
+    <a href="{{ config("app.url") }}" class="logo">MMCK</a>
+
     <form method="GET" action="{{ config('app.url') }}/search">
         <input type="text" name="q" placeholder="Search..." autocomplete="off">
     </form>
