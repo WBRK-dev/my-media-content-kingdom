39afe9516e68f1112c84719ed12488bae3e3ee07
WBRK-dev <koertjrweberp@gmail.com>
2024-05-22 10:00:46 +0200

    fix: fixed searchbar position

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="topnavbar">
     <form method="GET" action="{{ config('app.url') }}/search">
-        <input type="text" name="q" placeholder="Search..." style="margin: 0 auto; display:block; width: min(800px, 100%);" autocomplete="off">
+        <input type="text" name="q" placeholder="Search..." autocomplete="off">
     </form>
 </div>
