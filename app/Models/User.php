6192826dd8fd97f37789ac3505873335db4d62ce
Team64OO <97109976@st.deltion.nl>
2024-05-24 10:46:16 +0200

    feat: added laratrust

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laratrust\Contracts\LaratrustUser;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\HasRolesAndPermissions;
 
-class User extends Authenticatable
+class User extends Authenticatable implements LaratrustUser
 {
+    use HasRolesAndPermissions;
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
