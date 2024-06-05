e7177da4b519404669c7f2a6fe5a8188f9e6dad6
Team64OO <97109976@st.deltion.nl>
2024-06-05 09:13:43 +0200

    feat: added clear search bar button

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,7 +3,10 @@
     <a href="{{ config("app.url") }}" class="logo">MMCK</a>
 
     <form method="GET" action="{{ config('app.url') }}/search">
-        <input type="text" name="q" value="{{ isset($_GET['q']) ? $_GET['q'] : '' }}" placeholder="Search..." autocomplete="off">
+        <div class="d-flex">
+            <input type="text" id="search-bar" name="q" value="{{ isset($_GET['q']) ? $_GET['q'] : '' }}" placeholder="Search..." autocomplete="off">
+            <button type="button" onclick="clearSearchBar()">Clear</button>
+        </div>
         <button name="filter" value="release_date">Release Date</button>
         <button name="filter" value="length">Length</button>
         <button name="filter" value="views">Views</button>
@@ -19,3 +22,11 @@
         <a href="{{ config("app.url") }}/login">Login</a>
     @endif
 </div>
+
+<script>
+    
+    function clearSearchBar() {
+        let elem = document.getElementById("search-bar");
+        elem.value = "";
+    }
+</script>
