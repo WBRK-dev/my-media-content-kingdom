a3603ed10e3834db00022ed7e12b6ad5e52c684c
idontknow83 <manoncristel37@gmail.com>
2024-06-07 11:23:16 +0200

    feat: added navbar and sidebar CSS

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -4,14 +4,17 @@
 
     <form method="GET" action="{{ config('app.url') }}/search">
         <div class="d-flex">
-            <input type="text" id="search-bar" name="q" value="{{ isset($_GET['q']) ? $_GET['q'] : '' }}" placeholder="Search..." autocomplete="off">
-            <button type="button" onclick="clearSearchBar()">Clear</button>
+            <input type="text" id="search-bar" class="search-bar" name="q" value="{{ isset($_GET['q']) ? $_GET['q'] : '' }}" placeholder="Search..." autocomplete="off">
+            <button type="button"class="delete" onclick="clearSearchBar()"><span class="material-symbols-outlined">close </span></button>
         </div>
-        <button name="filter" value="release_date">Release Date</button>
-        <button name="filter" value="length">Length</button>
-        <button name="filter" value="views">Views</button>
-        <button name="filter" value="likes">Likes</button>
+        <div class="d-flex justify-content-center p-2 gap-4">
+            <button name="filter" class="filter-button" value="release_date">Release Date</button>
+            <button name="filter" class="filter-button" value="length">Length</button>
+            <button name="filter" class="filter-button" value="views">Views</button>
+            <button name="filter" class="filter-button" value="likes">Likes</button>
+        </div>
     </form>
+    
 
     @if (Auth::check())
         <div class="d-flex gap-2">
@@ -24,9 +27,35 @@
 </div>
 
 <script>
-    
-    function clearSearchBar() {
-        let elem = document.getElementById("search-bar");
-        elem.value = "";
-    }
+function clearSearchBar() {
+    let elem = document.getElementById("search-bar");
+    elem.value = "";
+    console.log("test");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const searchBar = document.querySelector('.search-bar');
+    const searchBarInput = document.querySelector('.search-bar-input');
+
+    searchBarInput.addEventListener('focus', function() {
+        searchBar.classList.add('active');
+    });
+
+    searchBarInput.addEventListener('blur', function() {
+        searchBar.classList.remove('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const deleteElement = document.querySelector('.delete');
+    const deleteInput = document.querySelector('.delete-input');
+
+    deleteInput.addEventListener('focus', function() {
+       deleteElement.classList.add('active');
+    });
+
+    deleteInput.addEventListener('blur', function() {
+        deleteElement.classList.remove('active');
+    });
+});
 </script>
