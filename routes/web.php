a483525c71ba02f172435fda3876efaeba6c6b8a
WBRK-dev <koertjrweberp@gmail.com>
2024-06-13 08:46:02 +0200

    chore: cleaned the web routes

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,18 @@ use App\Models\Video;
 
 require __DIR__.'/auth.php';
 
+// Home
 Route::get("/", [HomeController::class, "show"])->name("home");
 
+// Search
 Route::get('/search', [SearchController::class, "search"]);
 
+// Upload
 Route::get("/upload", [VideoController::class, "create"])->middleware("auth");
 Route::post("/upload", [VideoController::class, "store"])->middleware("auth");
 Route::post("/upload-youtube", [VideoController::class, "storeFromYoutube"])->middleware("auth");
 
-Route::delete("/delete/{video}", [VideoController::class, "destroy"]);
-
+// Watch
 Route::get("/watch", [WatchController::class, "show"]);
 
 Route::get("/watch/{videoid}/{resolution}/{filename}", [VideoController::class, "show"]);
@@ -43,11 +45,12 @@ Route::get("/api/watch/disliked", [WatchController::class, "videoDisliked"]);
 Route::get("/api/watch/like_rem_row", [WatchController::class, "deleteLikeRow"]);
 Route::post("/api/watch/report_video", [WatchController::class, "videoReported"]);
 
+// Admin
 Route::get("/reported-videos", [ReportedVideosController::class, "show"]);
 
+// Account
 Route::view("/login", "account.login")->middleware("nonauth");
 Route::view("/register", "account.register")->middleware("nonauth");
 
+// API
 Route::get("/api/thumbnail", [ThumbnailController::class, "show"]);
-
-Route::get("/test", [VideoController::class, "test"]);
