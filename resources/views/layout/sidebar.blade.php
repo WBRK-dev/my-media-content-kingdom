744f42dfe22c0eec5ba32e639c8756d331a02127
WBRK-dev <koertjrweberp@gmail.com>
2024-06-13 14:50:37 +0200

    fix: added correct linking to channel in sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -3,15 +3,15 @@
         <span class="material-symbols-outlined">home</span>
         <a href="{{ config('app.url') }}">Home</a>
     </div>
-    <div class="sidebar-item">
-        <span class="material-symbols-outlined">person</span>
-        <a href="{{ config('app.url') }}">Your Channel</a>
-    </div>
-    <div class="sidebar-item last-item">
-        <span class="material-symbols-outlined">playlist_play</span>
-        <a href="{{ config('app.url') }}">Playlists</a>
-      
- 
-    </div>
+    @auth
+        <div class="sidebar-item">
+            <span class="material-symbols-outlined">person</span>
+            <a href="{{ config('app.url') }}/channel/{{ Auth::user()->id }}">Your Channel</a>
+        </div>
+        <div class="sidebar-item last-item">
+            <span class="material-symbols-outlined">playlist_play</span>
+            <a href="{{ config('app.url') }}">Playlists</a>
+        </div>
+    @endauth
     
 </div>
