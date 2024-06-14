d7e1d58ac64d7d353b194ca791caadf5b2702c8f
WBRK-dev <koertjrweberp@gmail.com>
2024-06-14 09:16:57 +0200

    fix: fixed channel name etc sticking to bottom of video card

--- resources/views/channel/home.blade.php
+++ resources/views/channel/home.blade.php
@@ -7,7 +7,7 @@
         <video-grid class="w-100 single-row">
             @foreach ($channel->videos()->orderBy("created_at", "desc")->limit(10)->get() as $video)
                 <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
-                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
+                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
                         <div class="img-wrapper">
                             <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
                             <p class="tag">{{ $video->shortDuration() }}</p>
@@ -46,7 +46,7 @@
         <video-grid class="w-100 single-row">
             @foreach ($data["most_liked"] as $video)
                 <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
-                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
+                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
                         <div class="img-wrapper">
                             <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
                             <p class="tag">{{ $video->shortDuration() }}</p>
@@ -97,7 +97,7 @@
         <video-grid class="w-100 single-row">
             @foreach ($data["most_viewed"] as $video)
                 <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
-                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}">
+                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
                         <div class="img-wrapper">
                             <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
                             <p class="tag">{{ $video->shortDuration() }}</p>
