a3690dfce767dafbe18e83179b019a7e9fc136cd
WBRK-dev <koertjrweberp@gmail.com>
2024-06-15 20:42:33 +0200

    fix: optimize account popup event listener

--- public/script-modules/account-popup.js
+++ public/script-modules/account-popup.js
@@ -8,6 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
     accountPopup = document.querySelector(".account-dropdown");
 });
 document.addEventListener("click", function(e) {
-    if (e.target.closest(".account-dropdown-wrapper") || !accountPopup.classList.contains("show")) return;
+    if (!accountPopup.classList.contains("show") || e.target.closest(".account-dropdown-wrapper")) return;
     accountPopup.classList.remove("show");
 });
