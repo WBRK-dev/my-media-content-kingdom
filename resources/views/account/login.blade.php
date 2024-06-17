47a855ff505dc2604fa30012f85da36835b1ab5a
idontknow83 <manoncristel37@gmail.com>
2024-06-17 13:32:41 +0200

    feat: added login page css

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -4,7 +4,6 @@
     
     <form action="{{ config("app.url") }}/login" method="post">
         @csrf
-
         <input type="text" name="email" placeholder="Email" autocomplete="off"><br>
         <input type="password" name="password" placeholder="Password" autocomplete="off"><br>
         <button>Login</button>
