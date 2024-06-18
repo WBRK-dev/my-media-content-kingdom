5d9dc4df95cab9caf1d6501e780ef0215a25572a
WBRK-dev <koertjrweberp@gmail.com>
2024-06-18 08:43:33 +0200

    feat: added message in channel page if channel has no videos

--- resources/views/channel/home.blade.php
+++ resources/views/channel/home.blade.php
@@ -2,133 +2,141 @@
 
 @section("channel-content")
     
-    <div>
-        <p class="fs-5 fw-bold mb-1">Recently Uploaded</p>
-        <video-grid class="w-100 single-row">
-            @foreach ($data["recently"] as $video)
-                <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
-                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
-                        <div class="img-wrapper">
-                            <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
-                            <p class="tag">{{ $video->shortDuration() }}</p>
-                        </div>
-                        <div class="info">
-                            <p class="title mb-2">{{ $video->title  }}</p>
-                            <div class="d-flex justify-content-between mt-auto">
-                                <div>
-                                    <div>{{$video->owner->name}}</div>
+    @if (count($data["recently"]) > 0)
+        
+        <div>
+            <p class="fs-5 fw-bold mb-1">Recently Uploaded</p>
+            <video-grid class="w-100 single-row">
+                @foreach ($data["recently"] as $video)
+                    <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
+                        <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
+                            <div class="img-wrapper">
+                                <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
+                                <p class="tag">{{ $video->shortDuration() }}</p>
+                            </div>
+                            <div class="info">
+                                <p class="title mb-2">{{ $video->title  }}</p>
+                                <div class="d-flex justify-content-between mt-auto">
                                     <div>
-                                        {{$video->getViews()}}
-    
-                                        @if ($video->getViews() == 1)
-                                            view
-                                        @else
-                                            views 
-                                        @endif
-    
-                                        &#x2022; {{$video->getTimeAgo()}}
-                                        
-                                        @if ($video->isNew())
-                                            <div class="new-badge">NEW</div>
-                                        @endif
+                                        <div>{{$video->owner->name}}</div>
+                                        <div>
+                                            {{$video->getViews()}}
+        
+                                            @if ($video->getViews() == 1)
+                                                view
+                                            @else
+                                                views 
+                                            @endif
+        
+                                            &#x2022; {{$video->getTimeAgo()}}
+                                            
+                                            @if ($video->isNew())
+                                                <div class="new-badge">NEW</div>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-                </video-grid-item>
-            @endforeach
-        </video-grid>
-    </div>
+                        </a>
+                    </video-grid-item>
+                @endforeach
+            </video-grid>
+        </div>
 
-    <div>
-        <p class="fs-5 fw-bold mb-1">Most Liked</p>
-        <video-grid class="w-100 single-row">
-            @foreach ($data["most_liked"] as $video)
-                <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
-                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
-                        <div class="img-wrapper">
-                            <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
-                            <p class="tag">{{ $video->shortDuration() }}</p>
-                        </div>
-                        <div class="info">
-                            <p class="title mb-2">{{ $video->title  }}</p>
-                            <div class="d-flex justify-content-between mt-auto">
-                                <div>
-                                    <div>{{$video->owner->name}}</div>
+        <div>
+            <p class="fs-5 fw-bold mb-1">Most Liked</p>
+            <video-grid class="w-100 single-row">
+                @foreach ($data["most_liked"] as $video)
+                    <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
+                        <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
+                            <div class="img-wrapper">
+                                <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
+                                <p class="tag">{{ $video->shortDuration() }}</p>
+                            </div>
+                            <div class="info">
+                                <p class="title mb-2">{{ $video->title  }}</p>
+                                <div class="d-flex justify-content-between mt-auto">
                                     <div>
-                                        {{$video->likes ?? 0}}
-    
-                                        @if ($video->likes == 1)
-                                            like
-                                        @else
-                                            likes 
-                                        @endif
-    
-                                        &#x2022; 
+                                        <div>{{$video->owner->name}}</div>
+                                        <div>
+                                            {{$video->likes ?? 0}}
+        
+                                            @if ($video->likes == 1)
+                                                like
+                                            @else
+                                                likes 
+                                            @endif
+        
+                                            &#x2022; 
 
-                                        {{$video->getViews()}}
-    
-                                        @if ($video->getViews() == 1)
-                                            view
-                                        @else
-                                            views 
-                                        @endif
-                                        
-                                        &#x2022; 
+                                            {{$video->getViews()}}
+        
+                                            @if ($video->getViews() == 1)
+                                                view
+                                            @else
+                                                views 
+                                            @endif
+                                            
+                                            &#x2022; 
 
-                                        {{$video->getTimeAgo()}}
-                                        
-                                        @if ($video->isNew())
-                                            <div class="new-badge">NEW</div>
-                                        @endif
+                                            {{$video->getTimeAgo()}}
+                                            
+                                            @if ($video->isNew())
+                                                <div class="new-badge">NEW</div>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-                </video-grid-item>
-            @endforeach
-        </video-grid>
-    </div>
+                        </a>
+                    </video-grid-item>
+                @endforeach
+            </video-grid>
+        </div>
 
-    <div>
-        <p class="fs-5 fw-bold mb-1">Most Viewed</p>
-        <video-grid class="w-100 single-row">
-            @foreach ($data["most_viewed"] as $video)
-                <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
-                    <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
-                        <div class="img-wrapper">
-                            <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
-                            <p class="tag">{{ $video->shortDuration() }}</p>
-                        </div>
-                        <div class="info">
-                            <p class="title mb-2">{{ $video->title  }}</p>
-                            <div class="d-flex justify-content-between mt-auto">
-                                <div>
-                                    <div>{{$video->owner->name}}</div>
+        <div>
+            <p class="fs-5 fw-bold mb-1">Most Viewed</p>
+            <video-grid class="w-100 single-row">
+                @foreach ($data["most_viewed"] as $video)
+                    <video-grid-item class="{{ $video->isFromYoutube() ? "youtube" : "" }}">
+                        <a href="{{ config('app.url') }}/watch?id={{ $video->getId() }}" class="h-100 d-flex flex-column">
+                            <div class="img-wrapper">
+                                <img src="{{ config('app.url') }}/api/thumbnail?id={{ $video->thumbnail->id }}" class="video-thumbnail" style="width: 100%;">
+                                <p class="tag">{{ $video->shortDuration() }}</p>
+                            </div>
+                            <div class="info">
+                                <p class="title mb-2">{{ $video->title  }}</p>
+                                <div class="d-flex justify-content-between mt-auto">
                                     <div>
-                                        {{$video->views ?? 0}}
-    
-                                        @if ($video->views == 1)
-                                            view
-                                        @else
-                                            views 
-                                        @endif
-    
-                                        &#x2022; {{$video->getTimeAgo()}}
-                                        
-                                        @if ($video->isNew())
-                                            <div class="new-badge">NEW</div>
-                                        @endif
+                                        <div>{{$video->owner->name}}</div>
+                                        <div>
+                                            {{$video->views ?? 0}}
+        
+                                            @if ($video->views == 1)
+                                                view
+                                            @else
+                                                views 
+                                            @endif
+        
+                                            &#x2022; {{$video->getTimeAgo()}}
+                                            
+                                            @if ($video->isNew())
+                                                <div class="new-badge">NEW</div>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-                </video-grid-item>
-            @endforeach
-        </video-grid>
-    </div>
+                        </a>
+                    </video-grid-item>
+                @endforeach
+            </video-grid>
+        </div>
+
+    @else
+
+        <p class="text-center" style="color: var(--body-secondary-color);">This channel does not have any activity yet.</p>
+        
+    @endif
 
 @endsection
